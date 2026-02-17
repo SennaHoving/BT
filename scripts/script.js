9ab36cad1ffdef05701730698ac02c9370aa5cce
SennaHoving <senna.hoving@gmail.com>
2026-02-17 11:28:54 +0100

    linstener op alle radio buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,15 @@
-const radioButtons = document.querySelectorAll("input[type=radio]"); 
-
 document.addEventListener("change", function(e) {
     if (e.target.matches('input[type="radio"]')) {
-        console.log(e.target.value)
-        //if target.value == Ja 
-        //dan remove class van de target.dataset.target
+        const targetId = document.getElementById(e.target.dataset.target)
+
+        if(e.target.value == "Ja") {
+            console.log(targetId)
+            targetId.classList.remove("hidden")
+            //maak het deel hierbij ook nog required? 
+        } else {
+            targetId.classList.add("hidden")
+        }
     }
 })
+
+
