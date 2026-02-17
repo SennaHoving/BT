92319b0e3158c14dee7ee50b43146aeeae5edfc1
SennaHoving <senna.hoving@gmail.com>
2026-02-17 11:58:03 +0100

    character preventies text fields

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,22 @@
+const bsnField = document.querySelector(".bsn") 
+const textFields = document.querySelectorAll(".text_only")
+
+//Prevent characters
+//Number fields
+bsnField.addEventListener("keydown", event => {
+    if(isNaN(event.key) && event.key !== 'Backspace') {
+        event.preventDefault();
+    }
+})
+
+//Text fields
+textFields.forEach(field => {
+    field.addEventListener("input", () => {
+        field.value = field.value.replace(/[^\p{L}]/gu, "");
+    })
+})
+
+//Listener radio buttons 
 document.addEventListener("change", function(e) {
     if (e.target.matches('input[type="radio"]')) {
         const targetId = document.getElementById(e.target.dataset.target)
