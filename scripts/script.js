9f17ec805e88751a9759ec373c94ce11214270aa
SennaHoving <senna.hoving@gmail.com>
2026-03-02 15:04:09 +0100

    buttons verder gefixed

--- scripts/script.js
+++ scripts/script.js
@@ -102,6 +102,7 @@ function createNewVerkrijger() {
     const radio1 = document.createElement('input');
     radio1.setAttribute('value', 'Ja');  
     radio1.setAttribute('type', 'radio');
+    radio1.setAttribute('name', 'vermogen');
 
     labelRadio1.prepend(radio1)
 
@@ -111,9 +112,9 @@ function createNewVerkrijger() {
     const radio2 = document.createElement('input');
     radio2.setAttribute('value', 'Ja');  
     radio2.setAttribute('type', 'radio'); 
+    radio2.setAttribute('name', 'vermogen');
 
     labelRadio2.prepend(radio2)
-
 
     // Vraag 2 
     const fieldSetV2 = document.createElement("fieldset") 
@@ -122,22 +123,24 @@ function createNewVerkrijger() {
     fieldSetV2.appendChild(legendV2)
 
     const labelRadio3 = document.createElement('label')
-    labelRadio3.setAttribute('for', 'radio3');
     labelRadio3.innerText = 'Nee'; 
 
     const radio3 = document.createElement('input');
     radio3.setAttribute('value', 'Ja');  
     radio3.setAttribute('type', 'radio');
-    radio3.id = 'radio3'
+    radio3.setAttribute('name', 'legitieme');
+
+    labelRadio3.prepend(radio3)
 
     const labelRadio4 = document.createElement('label')
-    labelRadio4.setAttribute('for', 'radio4 ');
     labelRadio4.innerText = 'Ja'; 
 
     const radio4 = document.createElement('input');
     radio4.setAttribute('value', 'Ja');  
-    radio4.setAttribute('type', 'radio');
-    radio4.id = 'radio4'
+    radio4.setAttribute('type', 'radio'); 
+    radio4.setAttribute('name', 'legitieme');
+    
+    labelRadio4.prepend(radio4)
 
     //Remove button
     const remove = document.createElement("button") 
@@ -159,11 +162,9 @@ function createNewVerkrijger() {
 
     fieldSetV1.appendChild(labelRadio1)
     fieldSetV1.appendChild(labelRadio2)
-    
-    fieldSetV2.appendChild(labelRadio3)
-    fieldSetV2.appendChild(radio3) 
+
+    fieldSetV2.appendChild(labelRadio3) 
     fieldSetV2.appendChild(labelRadio4)
-    fieldSetV2.appendChild(radio4) 
 
     newFieldset.appendChild(fieldSetV1)
     newFieldset.appendChild(fieldSetV2)
